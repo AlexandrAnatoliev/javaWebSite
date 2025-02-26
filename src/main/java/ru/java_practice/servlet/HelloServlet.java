0f5599c21c675ce5b28f4f97b2f3c3b302e9a3cb
AlexandrAnatoliev <per-1986@list.ru>
2025-02-26 21:13:03 +0300

    fix: run helloServlet

--- src/main/java/ru/java_practice/servlet/HelloServlet.java
+++ src/main/java/ru/java_practice/servlet/HelloServlet.java
@@ -13,6 +13,7 @@ import java.io.PrintWriter;
 public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
         PrintWriter printWriter = resp.getWriter();
         printWriter.write("Hello servlet!!!");
         printWriter.close();

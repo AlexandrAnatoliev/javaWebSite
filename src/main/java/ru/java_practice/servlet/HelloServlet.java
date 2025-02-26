1e1b69ef58e840fb3cfb6b9d90a217b7dbdf4aa4
AlexandrAnatoliev <per-1986@list.ru>
2025-02-26 22:40:04 +0300

    fix: add helloServlet by web.xml

--- src/main/java/ru/java_practice/servlet/HelloServlet.java
+++ src/main/java/ru/java_practice/servlet/HelloServlet.java
@@ -9,11 +9,11 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/hello")
+//@WebServlet("/hello")
 public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/html");
+        //resp.setContentType("text/html");
         PrintWriter printWriter = resp.getWriter();
         printWriter.write("Hello servlet!!!");
         printWriter.close();
